7a8ff154ac0cacf330192628eb0436144a7786d5
sudin <www.sudin@gmail.com>
2016-06-01 23:00:01 +0545

    reindented

--- Sudin/Ruby_Day2/factorial.rb
+++ Sudin/Ruby_Day2/factorial.rb
@@ -1,27 +1,37 @@
-require'rspec'
+	require'rspec'
 
-describe "Factorial" do
-	it "2 should give 2" do
-		factorial = Factorial.new(2)
-		result = factorial.calculate
-		expect(result).to eq(2)
-	end
-end
-
-
-class Factorial
-	def initialize(number)
-		@num = number
-	end
-
-	def calculate
-	   result = 1
-	   while @num>1 do
-	   	result = result * @num
-	   	@num = @num -1   
+	describe "Factorial" do
+		it "2 should give 2" do
+			factorial = Factorial.new(2)
+			result = factorial.calculate
+			expect(result).to eq(2)
 		end
-		result
+		it "5 should give 120" do
+			factorial = Factorial.new(5)
+			result = factorial.calculate
+			expect(result).to eq(120)
+		end
+		it " 0 should give 1" do
+			factorial = Factorial.new(0)
+			result = factorial.calculate
+			expect(result).to eq(1)
+		end
 	end
-end
+
+
+	class Factorial
+		def initialize(number)
+			@num = number
+		end
+
+		def calculate
+		   result = 1
+		   while @num>1 do
+		   	result = result * @num
+		   	@num = @num -1   
+			end
+			result
+		end
+	end
 
 
