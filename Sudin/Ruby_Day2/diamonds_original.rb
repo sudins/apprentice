0e95abbf992a658f4c8a0aee761e14e462cd7787
sudin <www.sudin@gmail.com>
2016-06-01 23:16:43 +0545

    Test case only

---
+++ Sudin/Ruby_Day2/diamonds_original.rb
@@ -0,0 +1,21 @@
+require 'rspec'
+
+describe 'diamond designs' do
+	it "person 1 should get 1 and 4 arrays" do
+		expect().to eq[1,4]
+	end
+
+	it "person 2 should get 2 and 3 arrays"
+		expect().to eq[2,3]
+	end
+
+	it "person 3 should get 5"
+		expect().to eq[5]
+	end
+end
+
+class Array
+	arr = [1,2,3,4,5]
+	arr[0] + arr[3]
+	
+end
