b854619a4f9392dd6efbf42f87a30c47493408eb
sudin <www.sudin@gmail.com>
2016-06-02 17:27:10 +0545

    TDD for area and exception written and verified

--- Sudin/Ruby_day3/area.rb
+++ Sudin/Ruby_day3/area.rb
@@ -1,3 +1,17 @@
+require 'rspec'
+
+describe "Shape" do
+	it "should give area" do
+		expect(Area.new(1,1,1).calc).to eq(1)
+	end
+	it "should give circle" do
+		expect(Circle.new(1,1,1).calc).to eq(3.14)
+	end
+	it "should give rectangle" do
+		expect(Rect.new(1,1,1).calc).to eq(1)
+	end
+end
+
 class Shape
 	def initialize(l,b,r)
 		@l = l
@@ -10,18 +24,17 @@ class Area  < Shape
 		result = @l * @l
 	end
 end
-
 class Rect  < Shape
 	def calc
 		result = @l * @b
 	end
 end
-
 class Circle < Shape
 	def calc
 		result = 3.14 * @r * @r
 	end
 end
+=begin
 
 s= Circle.new(01,01,10)
 s.calc
@@ -34,3 +47,4 @@ puts "the area is #{s.calc}"
 s= Rect.new(10,20,0)
 s.calc
 puts "the rectangle is #{s.calc}"
+=end
--- Sudin/Ruby_day3/exception1.rb
+++ Sudin/Ruby_day3/exception1.rb
@@ -1,48 +1,38 @@
-
 require 'rspec'
 
-class ZeroDivision
+describe "ZeroDivision error" do
+	context 'true case'do
+		it "should give no ZeroDivision error"do
+			expect(ZeroDivision.new(3,3).divison).to eq(1)
+		end
+		it "should give no ZeroDivision error"do
+			expect(ZeroDivision.new(4,4).divison).to eq(1)
+		end
+	end
 
+	context 'false case'do
+		it "should give ZeroDivision error"do
+			expect(ZeroDivision.new(3,0).divison).to eq(nil)
+		end
+		it "should give ZeroDivision error"do
+			expect(ZeroDivision.new(4,0).divison).to eq(nil)
+		end
+	end
+end
+
+class ZeroDivision
 	def initialize(num1,num2)
 		@num1 = num1
 		@num2 = num2
 	end
 	
-	def division
+	def divison
 		begin
 			result = @num1/@num2
 		rescue
 			if @num2 == 0
-				puts "error made by zero"	
 			end
 		end
-
 	end
-end
-obj = ZeroDivision.new(4,4)
-obj.divison
-
-describe "zerodivision error" do
-	context 'true case'do
-	it "should give no ZeroDivision error"do
-
-	expect(ZeroDivision.new(3,3).divison).to eq(1)
-
-end
-it "should give no ZeroDivision error"do
-expect(ZeroDivision.new(4,4).divison).to eq(1)
-end
-
-end
-
-context 'false case'do
-it "should give  ZeroDivision error"do
-expect(ZeroDivision.new(3,0).divison).to eq("NA")
-end
-it "should give ZeroDivision error"do
-expect(ZeroDivision.new(3,0).divison).to eq("NA")
-end
-
-end
 
 end
