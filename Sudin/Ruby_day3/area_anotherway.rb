f0c7bbbd91c1ddabb2c9329c84e23405a34b6257
sudin <www.sudin@gmail.com>
2016-06-02 18:00:49 +0545

    added another area method

---
+++ Sudin/Ruby_day3/area_anotherway.rb
@@ -0,0 +1,47 @@
+require 'rspec'
+
+describe "Shape" do
+	it "should give circle" do
+		expect(Circle.new(1,1,1).calc).to eq(3.14)
+	end
+	it "should give rectangle" do
+		expect(Rect.new(1,1,1).calc).to eq(1)
+	end
+end
+
+class Shape
+	def initialize(l,b,r)
+		@l = l
+		@b = b
+		@r = r
+	end
+	
+
+	def area_calc
+		result = @l * @l
+	end
+end
+class Rect  < Shape
+	def calc
+		result = @l * @b
+	end
+end
+class Circle < Shape
+	def calc
+		result = 3.14 * @r * @r
+	end
+end
+=begin
+
+s= Circle.new(01,01,10)
+s.calc
+puts "the radius is #{s.calc}"
+
+s= Area.new(10,20,0)
+s.calc
+puts "the area is #{s.calc}"
+
+s= Rect.new(10,20,0)
+s.calc
+puts "the rectangle is #{s.calc}"
+=end
